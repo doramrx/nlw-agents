32fc735d70de1f57636078d0249940f956fc2ed5
Isadora Firmo <isah3027@gmail.com>
2025-07-10 20:45:45 -0300

    feat: add create question, create room and get room questions

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,10 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 import { env } from './env.ts';
+import { createRoomRoute } from './http/routes/create-room.ts';
+import { getRoomQuestionsRoute } from './http/routes/get-room-questions.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
+import { createQuestionRoute } from './http/routes/create-question.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -21,6 +24,9 @@ app.get('/health', () => {
   return 'OK';
 });
 
-app.register(getRoomsRoute)
+app.register(getRoomsRoute);
+app.register(createRoomRoute);
+app.register(getRoomQuestionsRoute);
+app.register(createQuestionRoute);
 
-app.listen({ port: env.PORT })
+app.listen({ port: env.PORT });
